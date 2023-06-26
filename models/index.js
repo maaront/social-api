31524fce29c933faf1573b29a40689db786e3591
maaront <35611834+maaront@users.noreply.github.com>
2023-06-26 17:16:55 -0400

    set up user schemas

---
+++ models/index.js
@@ -0,0 +1,5 @@
+const reactions = require('./reactions');
+const thoughts = require('./thoughts');
+const users = require('./users');
+
+module.exports = { reactions, thoughts, users };
---
+++ models/users.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/.+@.+\..+/] // use regex to validate correct email format
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    });
