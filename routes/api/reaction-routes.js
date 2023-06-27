648487114a5cb1b8ae0071406e0817a9d7d365a5
maaront <35611834+maaront@users.noreply.github.com>
2023-06-27 16:15:13 -0400

    merge reaction route logic to thought route

--- routes/api/reaction-routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-const {
-    addReaction,
-    removeReaction
-} = require('../../controllers/user-controller');
-
-module.exports = router;
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -6,6 +6,27 @@ const {
     createThought,
     updateThought,
     deleteThought,
+    addReaction,
+    removeReaction
 } = require('../../controllers/thought-controller');
+
+router
+.route('/')
+.get(getAllThoughts);
+
+router
+.route('/:id')
+.get(getThoughtById)
+.put(updateThought)
+.delete(deleteThought);
+
+router
+.route('/:userId')
+.post(createThought);
+
+router
+.route('/:thoughtId/reactions')
+.post(addReaction)
+.delete(removeReaction);
 
 module.exports = router;
