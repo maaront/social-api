623d192c62c1fe565ece11f7ec6fb568aaf2b2ee
maaront <35611834+maaront@users.noreply.github.com>
2023-06-27 15:26:29 -0400

    add additional heirachry

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const db = require('./config/connection');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(/api/routes);
