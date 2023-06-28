354eca184f07027ab730b8e1300c5a3d6e211eae
maaront <35611834+maaront@users.noreply.github.com>
2023-06-28 16:31:53 -0400

    add back reaction model and tweak user and thought models to work

--- controllers/tought-controller.js
+++
@@ -1,7 +0,0 @@
-const { connect, connection } = require('mongoose');
-
-const connectionString = process.env.MONGODB_URI || 'mongodb://localhost/social-api';
-
-connect(connectionString);
-
-module.exports = connection;
---
+++ controllers/user-controller.js
@@ -0,0 +1,17 @@
+const User = require('../models/User');
+const Thought = require('../models/Thought');
+const userController = {
+
+    getAllUsers: async (req, res) => {
+        try {
+            const dbUserData = await User.find({});
+            res.json(dbUserData);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Error getting all users', details: err.message });
+        }
+    },
+};
+
+
+module.exports = userController;
