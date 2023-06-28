bd2259a1abc0250c2c57cb5952b7bd19c922e0bb
maaront <35611834+maaront@users.noreply.github.com>
2023-06-28 16:39:34 -0400

    fix typos in model index

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-const Reaction = require('./reaction');
-const Thought = require('./thought');
-const User = require('./user');
+const reaction = require('./reaction');
+const thought = require('./thought');
+const user = require('./user');
 
 
 module.exports = { reaction, thought, user };
